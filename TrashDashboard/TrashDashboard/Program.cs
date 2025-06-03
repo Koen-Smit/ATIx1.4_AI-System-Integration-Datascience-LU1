7796a8871e59521883281f96f699348c9f6399a9
KerrieMeister96 <koensmit13@gmail.com>
2025-06-04 01:38:35 +0200

    #67

--- TrashDashboard/TrashDashboard/Program.cs
+++ TrashDashboard/TrashDashboard/Program.cs
@@ -8,9 +8,12 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+
         // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
+
+        builder.Services.AddHttpClient();
 
         var app = builder.Build();
 
---
+++ TrashDashboard/TrashDashboard/wwwroot/js/chart-init.js
@@ -0,0 +1,34 @@
+﻿window.drawCharts = (plasticData, dateData) => {
+    new Chart(document.getElementById("plasticChart"), {
+        type: 'bar',
+        data: {
+            labels: plasticData.labels,
+            datasets: [{
+                label: 'Kg plastic',
+                data: plasticData.data,
+                backgroundColor: 'rgba(75, 192, 192, 0.6)'
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+
+    new Chart(document.getElementById("dateChart"), {
+        type: 'bar',
+        data: {
+            labels: dateData.labels,
+            datasets: [{
+                label: 'Kg afval',
+                data: dateData.data,
+                backgroundColor: 'rgba(153, 102, 255, 0.6)'
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: { ticks: { maxRotation: 90, minRotation: 45 } }
+            }
+        }
+    });
+};
