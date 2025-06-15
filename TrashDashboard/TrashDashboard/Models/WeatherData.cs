8b890734f13f7d123f93a1bce5f5a3fdf6189ef9
ZeeSterre <sterre17032005@gmail.com>
2025-06-15 11:02:55 +0200

    Mobile responsive #85

---
+++ TrashDashboard/TrashDashboard/Models/WeatherData.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrashDashboard.Models
+{
+    public class WeatherData
+    {
+        public string[] Dates { get; set; } = Array.Empty<string>();
+        public double[] Temperaturen { get; set; } = Array.Empty<double>();
+        public string[] WeerOmschrijvingen { get; set; } = Array.Empty<string>();
+    }
+}
