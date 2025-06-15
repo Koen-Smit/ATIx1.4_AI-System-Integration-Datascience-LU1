f0407b541cd2ace695a9eaae8c22822e058602d9
ZeeSterre <sterre17032005@gmail.com>
2025-06-15 10:50:48 +0200

    Unit tests #86

---
+++ MonitoringAPI.Tests/AccountControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+[TestClass]
+public class AccountControllerTests
+{
+    private readonly Mock<UserManager<ApplicationUser>> _mockUserManager;
+    private readonly Mock<SignInManager<ApplicationUser>> _mockSignInManager;
+    private readonly AccountController _controller;
+
+    public AccountControllerTests()
+    {
+        var store = new Mock<IUserStore<ApplicationUser>>();
+        _mockUserManager = new Mock<UserManager<ApplicationUser>>(
+            store.Object, null, null, null, null, null, null, null, null);
+
+        var contextAccessor = new Mock<IHttpContextAccessor>();
+        var userPrincipalFactory = new Mock<IUserClaimsPrincipalFactory<ApplicationUser>>();
+        _mockSignInManager = new Mock<SignInManager<ApplicationUser>>(
+            _mockUserManager.Object,
+            contextAccessor.Object,
+            userPrincipalFactory.Object,
+            null, null, null, null);
+
+        _controller = new AccountController(
+            _mockUserManager.Object,
+            _mockSignInManager.Object,
+            null);
+    }
+
+    [TestMethod]
+    public async Task Register_WithValidData_ReturnsSuccess()
+    {
+        var dto = new RegisterDto { Email = "test@test.com", Password = "ValidPass123!" };
+        _mockUserManager.Setup(x => x.FindByEmailAsync(dto.Email))
+                       .ReturnsAsync((ApplicationUser)null);
+        _mockUserManager.Setup(x => x.CreateAsync(It.IsAny<ApplicationUser>(), dto.Password))
+                       .ReturnsAsync(IdentityResult.Success);
+
+        var result = await _controller.Register(dto);
+
+        Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+    }
+}
---
+++ MonitoringAPI.Tests/CameraControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
+
+[TestClass]
+public class CameraControllerTests
+{
+    private readonly Mock<ICameraRepository> _mockRepo;
+    private readonly CameraController _controller;
+
+    public CameraControllerTests()
+    {
+        _mockRepo = new Mock<ICameraRepository>();
+        _controller = new CameraController(_mockRepo.Object);
+    }
+
+    [TestMethod]
+    public async Task CreateCamera_ValidData_ReturnsCreated()
+    {
+        var camera = new Camera { Id = 1, Naam = "Test Camera" };
+        _mockRepo.Setup(repo => repo.AddAsync(It.IsAny<Camera>())).ReturnsAsync(camera);
+
+        var result = await _controller.Create(camera);
+
+        Assert.IsInstanceOfType(result, typeof(CreatedAtActionResult));
+        var createdAtActionResult = result as CreatedAtActionResult;
+        Assert.IsNotNull(createdAtActionResult);
+        Assert.AreEqual(201, createdAtActionResult.StatusCode);
+        Assert.AreEqual(camera.Id, (createdAtActionResult.Value as Camera).Id);
+    }
+
+    [TestMethod]
+    public async Task GetCamera_ExistingId_ReturnsCamera()
+    {
+        var camera = new Camera { Id = 1, Naam = "Test Camera" };
+        _mockRepo.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync(camera);
+
+        var result = await _controller.GetById(1) as OkObjectResult;
+        var resultCamera = result.Value as Camera;
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(camera.Id, resultCamera.Id);
+    }
+
+    [TestMethod]
+    public async Task DeleteCamera_ExistingId_ReturnsNoContent()
+    {
+        _mockRepo.Setup(repo => repo.DeleteAsync(1)).ReturnsAsync(true);
+
+        var result = await _controller.Delete(1);
+
+        Assert.IsInstanceOfType(result, typeof(NoContentResult));
+    }
+
+    [TestMethod]
+    public async Task GetCamera_NonExistingId_ReturnsNotFound()
+    {
+        _mockRepo.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync((Camera)null);
+
+        var result = await _controller.GetById(1);
+
+        Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+    }
+
+}
